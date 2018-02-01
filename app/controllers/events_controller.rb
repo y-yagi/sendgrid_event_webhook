77e77c04515ed00dfa38d0149144839bc08b0923
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2018-02-01 17:30:03 +0900

    add more log

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,5 +7,14 @@ class EventsController <  ApplicationController
 
   def create
     Rails.logger.error(params)
+    Rails.logger.error("JSON")
+    Rails.logger.error(params["_json"])
+    params[:_json].each do |event, value|
+      Rails.logger.error event
+      Rails.logger.error value
+    end
+
+    event = params["_json"].first
+    Rails.logger.error(event["sg_message_id"])
   end
 end
